3662cf73064005a3e28bb6d7a2cc276c4127baf7
Sukhbat.B <sukhbat.b@netcapital.mn>
2025-08-16 20:57:16 +0800

    render deploy хийхэд port гаднаас дамжуулах юм байна

--- python-openai/main.py
+++ python-openai/main.py
@@ -120,4 +120,4 @@ def get_evaluations():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # Render-аас ирсэн PORT-г ашиглана
-    uvicorn.run("main:app", host="127.0.0.1", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
